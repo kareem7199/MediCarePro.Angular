52ac05e5d6d5a4662f0a21df1c073bfbb87b4bef
kareem7199 <kareemtameregy@gmail.com>
2024-07-15 19:25:39 +0300

    fix : schedule bug

--- src/app/shared/components/schedule/schedule.component.ts
+++ src/app/shared/components/schedule/schedule.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhysicianSchedule } from '../../models/PhysicianSchedule.model';
 
 interface SelectedTime {
@@ -17,7 +17,7 @@ interface Visit {
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css'],
 })
-export class ScheduleComponent {
+export class ScheduleComponent implements OnInit {
   timeSelected: SelectedTime = {
     index: -5,
     patient: 'Kareem Tamer',
@@ -25,37 +25,19 @@ export class ScheduleComponent {
   };
 
   visits: Visit[] = [
-    {
-      patientName: "Ahmed",
-      date: new Date('2024-07-13T14:30:00'),
-    },
-    {
-      patientName: "Mahmoud",
-      date: new Date('2024-07-13T16:30:00'),
-    },
+    { patientName: 'Ahmed', date: new Date('2024-07-13T14:30:00') },
+    { patientName: 'Mahmoud Alaa', date: new Date('2024-07-16T17:30:00') },
+    { patientName: 'Hesham Mohammed', date: new Date('2024-07-16T16:30:00') },
+    { patientName: 'Mahmoud Samir', date: new Date('2024-07-16T13:30:00') },
   ];
 
   days: Date[] = [];
   skipDays = 0;
   physicianSchedule: PhysicianSchedule[] = [
-    {
-      id: 1,
-      day: 'Saturday',
-      startTime: '14:30:00',
-      endTime: '20:00:00',
-    },
-    {
-      id: 3,
-      day: 'Sunday',
-      startTime: '14:30:00',
-      endTime: '20:00:00',
-    },
-    {
-      id: 4,
-      day: 'Monday',
-      startTime: '15:30:00',
-      endTime: '20:00:00',
-    },
+    { id: 1, day: 'Saturday', startTime: '14:30:00', endTime: '20:00:00' },
+    { id: 3, day: 'Sunday', startTime: '14:15:00', endTime: '21:15:00' },
+    { id: 4, day: 'Monday', startTime: '15:30:00', endTime: '17:30:00' },
+    { id: 5, day: 'Tuesday', startTime: '13:30:00', endTime: '18:30:00' },
   ];
 
   startTime!: string;
@@ -63,45 +45,47 @@ export class ScheduleComponent {
   slots: string[] = [];
 
   ngOnInit(): void {
+    this.initializeSchedule();
+  }
+
+  private initializeSchedule() {
     this.generateDays();
     this.timeSelected.day = this.days[0];
     this.generateTimeSlots();
   }
-  generateDays() {
+
+  private generateDays() {
     this.days = [];
     for (let i = 0; i < 3; i++) {
       const result = new Date();
       result.setHours(0, 0, 0, 0);
-      result.setDate(result.getDate() + (i + this.skipDays * 3));
+      result.setDate(result.getDate() + i + this.skipDays * 3);
       this.days.push(result);
     }
   }
 
-  getEarliestAndLatestTimes() {
-    let earliestStartTime = this.physicianSchedule[0].startTime;
-    let latestEndTime = this.physicianSchedule[0].endTime;
+  private getEarliestAndLatestTimes() {
+    this.startTime = this.physicianSchedule.reduce(
+      (earliest, current) =>
+        current.startTime < earliest ? current.startTime : earliest,
+      this.physicianSchedule[0].startTime
+    );
 
-    this.physicianSchedule.forEach((entry) => {
-      if (entry.startTime < earliestStartTime) {
-        earliestStartTime = entry.startTime;
-      }
-      if (entry.endTime > latestEndTime) {
-        latestEndTime = entry.endTime;
-      }
-    });
-
-    this.startTime = earliestStartTime;
-    this.endTime = latestEndTime;
+    this.endTime = this.physicianSchedule.reduce(
+      (latest, current) =>
+        current.endTime > latest ? current.endTime : latest,
+      this.physicianSchedule[0].endTime
+    );
   }
 
-  generateTimeSlots() {
+  private generateTimeSlots() {
     this.getEarliestAndLatestTimes();
-
+    const slots = [];
     const start = new Date(`1970-01-01T${this.startTime}Z`);
     const end = new Date(`1970-01-01T${this.endTime}Z`);
-    const slots = [];
     const slotInterval = 15; // 15 minutes interval
 
+    start.setMinutes(end.getMinutes());
     end.setMinutes(end.getMinutes() + 2 * slotInterval);
 
     while (start <= end) {
@@ -113,18 +97,21 @@ export class ScheduleComponent {
   }
 
   selectTime(index: number, day: Date) {
-    if (!this.isSlotDisabled(day, index) && !this.isVisit(day , index) && !this.isVisit(day , index + 1) && !this.isVisit(day , index + 2)) {
+    if (
+      !this.isSlotDisabled(day, index) &&
+      !this.isVisit(day, index) &&
+      !this.isVisit(day, index + 1)
+    ) {
       this.timeSelected.day = day;
       this.timeSelected.index = index;
     }
   }
 
   isDisplayed(index: number, day: Date) {
-    if(this.isVisit(day, index - 1) || this.isVisit(day, index - 2)) return false;
     return (
-      !this.compareDates(this.timeSelected.day, day) ||
-      this.isSelected(index, day) ||
-      (index - this.timeSelected.index !== 2 &&
+      !this.isVisit(day, index - 1) &&
+      (!this.compareDates(this.timeSelected.day, day) ||
+        this.isSelected(index, day) ||
         index - this.timeSelected.index !== 1)
     );
   }
@@ -146,59 +133,63 @@ export class ScheduleComponent {
     this.generateDays();
   }
 
-  compareDates(d1: Date, d2: Date): boolean {
+  private compareDates(d1: Date, d2: Date): boolean {
     const time1 = d1.getTime();
     const time2 = d2.getTime();
     return time1 === time2;
   }
 
-  isSlotDisabled(day: Date, index: number) {
+  isSlotDisabled(day: Date, index: number): boolean {
     const dayString = this.getDayFromDate(day);
-    const isFound = this.physicianSchedule.find((PS) => PS.day === dayString)
-      ? false
-      : true;
-    return isFound || index >= this.slots.length - 2;
+    const physicianScheduleDay = this.physicianSchedule.find(
+      (PS) => PS.day === dayString
+    );
+
+    if (!physicianScheduleDay) return true;
+
+    const startTime = new Date(`1970-01-01T${physicianScheduleDay.startTime}Z`);
+    const endTime = new Date(`1970-01-01T${physicianScheduleDay.endTime}Z`);
+    const slotTime = new Date(`1970-01-01T${this.slots[index]}Z`);
+
+    return (
+      slotTime < startTime ||
+      slotTime > endTime ||
+      index >= this.slots.length - 2
+    );
   }
 
-  getDayFromDate(date: Date): string {
-    const options = { weekday: 'long' } as const; // Use 'short' for abbreviated day names if preferred
+  private getDayFromDate(date: Date): string {
+    const options = { weekday: 'long' } as const;
     return date.toLocaleDateString('en-US', options);
   }
 
-  getTimeFromDate(date: Date) {
+  private getTimeFromDate(date: Date): string {
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
     return `${hours}:${minutes}:${seconds}`;
   }
 
-  removeTimeFromDate(date: Date) {
-    date.setHours(0);
-    date.setMinutes(0);
-    date.setSeconds(0);
-    date.setMilliseconds(0);
+  private removeTimeFromDate(date: Date): Date {
+    date.setHours(0, 0, 0, 0);
     return date;
   }
 
-  areDatesEqualWithoutTime(date1: Date, date2: Date) {
-    // Set hours, minutes, seconds, and milliseconds to zero
-    const d1 = new Date(date1);
-    d1.setHours(0, 0, 0, 0);
-
-    const d2 = new Date(date2);
-    d2.setHours(0, 0, 0, 0);
-
-    // Compare dates without time
+  private areDatesEqualWithoutTime(date1: Date, date2: Date): boolean {
+    const d1 = this.removeTimeFromDate(new Date(date1));
+    const d2 = this.removeTimeFromDate(new Date(date2));
     return d1.getTime() === d2.getTime();
   }
 
-  isVisit(date: Date, index: number) {
-    let visit = this.getVisit(index , date);
-    if(visit) return true;
-    return false;
+  isVisit(date: Date, index: number): boolean {
+    return !!this.getVisit(index, date);
   }
 
-  getVisit(index : number , date : Date){
-    return this.visits.find(V => this.areDatesEqualWithoutTime(V.date, date) && index === this.slots.indexOf(this.getTimeFromDate(V.date)));
+  getVisit(index: number, date: Date): Visit | undefined {
+    return this.visits.find(
+      (visit) =>
+        this.areDatesEqualWithoutTime(visit.date, date) &&
+        index === this.slots.indexOf(this.getTimeFromDate(visit.date))
+    );
   }
 }
