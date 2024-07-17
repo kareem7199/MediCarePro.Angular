67de193334cfeccb2ed4e8c827b3a11126ea781c
kareem7199 <kareemtameregy@gmail.com>
2024-07-17 13:54:12 +0300

    feat: add ngOnDestroy to properly unsubscribe from subscriptions

--- src/app/features/visits/visits.component.ts
+++ src/app/features/visits/visits.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PhysicianScreenService } from 'src/app/core/services/physician-screen.service';
 import { DetailedVisit } from 'src/app/shared/models/DetailedVisit.model';
 import * as moment from 'moment';
 import { ToastrService } from 'ngx-toastr';
 import { VisitHubService } from 'src/app/core/services/visit-hub.service';
 import { Howl } from 'howler';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-visits',
   templateUrl: './visits.component.html',
   styleUrls: ['./visits.component.css'],
 })
-export class VisitsComponent implements OnInit {
+export class VisitsComponent implements OnInit , OnDestroy {
   date!: Date;
   selectedVisit: DetailedVisit | null = null;
   visits: DetailedVisit[] = [];
   loading = false;
+  private subscription!: Subscription;
   sound = new Howl({
     src: ['assets/sounds/notification.mp3'],
     volume: 1.0, // Adjust volume as needed (0.0 to 1.0)
@@ -27,22 +29,27 @@ export class VisitsComponent implements OnInit {
     private _toastr: ToastrService,
     private _visitHubService: VisitHubService
   ) {}
-
   ngOnInit(): void {
     this._visitHubService.startConnection();
 
-    this._visitHubService.visitAdded.subscribe({
+    this.subscription = this._visitHubService.visitAdded.subscribe({
       next: (response) => {
         const date1 = moment(this.date);
         const date2 = moment(response.date);
         if(date1.isSame(date2)) {
+          console.log(this.visits);
           this.visits.push(response.visit);
+          console.log(this.visits);
           this.visits.sort(this.sortByDate);
           this._toastr.success('New visit added!', 'Notification');
           this.sound.play();
         }
       }
     })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onSelectDate(event: any) {
